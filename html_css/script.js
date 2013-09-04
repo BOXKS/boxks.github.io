5c68e5c57c9e1c9504015e1d0753f10879479472
Kaiyuan <sealour@hotmail.com>
2013-09-05 01:16:11 +0800

    HTML&CSS 0.01

---
+++ html_css/script.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+    $('#list').on("click", "#list-button", function() {
+        $("#list").toggleClass('right0');
+    });
+});
---
+++ javascripts/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+	$('#btc-key').hover(function() {
+		$(this).select();
+	});
+	$('#btc>span').click(function() {
+		$("#btc>ul").toggleClass('block');
+	});
+	$("img").lazyload({
+	    effect : "fadeIn"
+	});
+});
