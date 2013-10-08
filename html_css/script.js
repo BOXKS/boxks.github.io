ceed030a8ac6956bb97f0bb0f2da380c3f212c18
Kaiyuan <sealour@hotmail.com>
2013-10-08 19:02:14 +0800

    Fixes bug
    修正一些载入页面的 BUG
    完善样式

--- html_css/script.js
+++ html_css/script.js
@@ -4,7 +4,6 @@ var PID = window.location.search;   //获取问号之后的部分
 var href = URL.substr(0,URL.lastIndexOf("/"));  //截取项目的目录
 var LID = PID.substr(1);    //截取问号
 var edition = "0_0_1";  //设置版本号（对应文件夹）
-// var list = $.getJSON("list.json");
 
 jQuery(document).ready(function($) {    
     //当页面框架载入完成后运行打括号内的代码
@@ -16,85 +15,113 @@ jQuery(document).ready(function($) {
         //如果载入的内容并非空白运行下面代码
         for (i = 0; i < list.length; i++) { 
             //设置始起的值是 0，每循环一次加1，当循环次数等于 list 中数组数量相同时候停止循环（因为 i 第一次为0，所以 i 最大值等于数组的数量减去1）
-            $('#list>#list-box>ol').append('<li><a id="'+list[i].LoadID+'" data-lid="'+i+'" href="'+href+'/?'+list[i].LoadID+'">'+list[i].title+'</a></li>');    //循环输出 li 菜单
+            $('#list>#list-box>ol').append('<li><a id="'+list[i].LoadID+'" data-listid="'+i+'" href="'+href+'/?'+list[i].LoadID+'">'+list[i].title+'</a></li>');    //循环输出 li 菜单
         };
     };
 
     function errorText () {
-        MainBox.html('<div class="warn center"><div class="e-mark"></div><br />網絡連接失敗，無法載入內容！</div>');
+        $('body').append('<div id="warn" class="center"><div><div class="e-mark"></div><br />網絡連接有誤，無法載入內容！</div></div>');
+        $('#warn').hide();
+        $('#warn').fadeIn("1000");
     }
 
     function LoadData(LoadPage) {   
         //载入页面内容的函数
+        if ($('#warn').length > 0) {
+            //  判断之前提示框是否存在
+            $('#warn').fadeOut("1000",function(){
+                $('#warn').detach();   //先淡出再删除提示框
+            });
+        };
+
         $.ajax({
             url: edition+"/"+LoadPage+".html",  //设置载入文件路径
             cache: false,
+            type: "post",
             beforeSend: function(){
                 //发送 Ajax 之前运行
-                loadingbar.width(0);
-                loadingbar.addClass('op1');
+                loadingbar.width(0);    //初始化进度条长度为0
+                loadingbar.addClass('op1'); //显示进度条
                 loadingbar.animate({
                     width: '30%'
-                }, "2000");
-                loadingbar.removeClass('warn');
+                }, "1000"); //修改进度条动画，设置长度为30%
+                loadingbar.removeClass('warn'); //保证进度条不是橘色
             },
+
             success: function(page){
                 //接受返回数据成功之后运行
+                LID = LoadPage;   //更新固定值
+
+                $("html,body").animate({scrollTop: $("#Main").offset().top}, 500);  //跳转到内容顶部
+
+                $('#list-box>ol>li>a').removeClass('current-cat');  // 清除目录的状态
+                $('#'+LID).addClass('current-cat'); //给当前目录中的当前条目添加状态
+
+                for (i = 0; i < list.length; i++) {
+                    if (list[i].LoadID===LoadPage) {
+                        $('title').html(list[i].title+' - 制作一个简单的网页'); // 遍历 json 设置对应标题
+                        break;  //终止循环
+                    };
+                };
+
                 loadingbar.animate({
                     width: '100%'
-                }, "2000");
+                }, "2000"); //改变进度条长度
                 MainBox.css("opacity","0");   //替换内容前先隐藏容器
                 setTimeout(function(){
                     //延时执行内容替换和显示
-                    $('#loadingbar').removeClass('op1')
-                    MainBox.html(page);
-                    MainBox.css('opacity','1')
+                    MainBox.html(page); // 替换主内容
+                    MainBox.css('opacity','1')  //显示主内容
                 },700);
+                setTimeout(function(){
+                    $('#loadingbar').removeClass('op1')
+                },1300);
             },
+
             error: function(){
                 //载入内容失败时候运行
                 loadingbar.animate({
                     width: '100%'
                 }, "2000");
-                loadingbar.addClass('warn');
-                MainBox.css("opacity","0");
+                loadingbar.addClass('warn');    //进度条变为橘色
                 setTimeout(function(){
-                    //延时执行内容替换和显示
-                    loadingbar.removeClass('op1')
-                    errorText (); 
-                    MainBox.css('opacity','1')
+                    errorText (); // 弹出错误提示筐
                 },700);
+                setTimeout(function(){
+                    $('#loadingbar').removeClass('op1') //隐藏进度条
+                },1300);
             }
         });
-
-
-        LID = LoadPage;   //更新固定值
-        $("html,body").animate({scrollTop: $("#Main").offset().top}, 500);  //跳转到内容顶部
-
-        $('#list-box>ol>li>a').removeClass('current-cat');  // 清除目录的状态
-        $('#'+LID).addClass('current-cat'); //给当前目录中的当前条目添加状态
-
-        for (i = 0; i < list.length; i++) {
-            if (list[i].LoadID===LoadPage) {
-                $('title').html(list[i].title+' - 制作一个简单的网页'); // 遍历 json 设置对应标题
-                break;  //终止循环
-            };
-        };
     }
 
     function HomePage () {
-        MainBox.load(edition+"/foreword.html");
-        LID = "foreword";   //设置默认页面状态
-        $('#list-box>ol>li>a').removeClass('current-cat');  //清除其他页面状态。
-        $('#'+LID).addClass('current-cat'); //添加当前页面状态。
-    }
+        //载入页面内容的函数
+        if ($('#warn').length > 0) {
+            //  判断之前提示框是否存在
+            $('#warn').fadeOut("1000",function(){
+                $('#warn').detach();   //先淡出再删除提示框
+            });
+        };
 
-    // // 判断 url 载入内容，有下面的 window.addEventListener 就不需要这里了！
-    // if (PID) {
-    // 	LoadData(LID);
-    // } else{
-    //     HomePage ();
-    // };
+        $.ajax({
+            url: edition+"/foreword.html",  //设置载入文件路径
+            cache: false,
+            type: "post",
+            success: function(page){
+                //接受返回数据成功之后运行
+                LID = "foreword";   //设置默认页面状态
+                $('#list-box>ol>li>a').removeClass('current-cat');  //清除其他页面状态。
+                $('#foreword').addClass('current-cat'); //添加当前页面状态。
+
+                MainBox.css("opacity","0");   //替换内容前先隐藏容器
+                setTimeout(function(){
+                    //延时执行内容替换和显示
+                    MainBox.html(page); // 替换主内容
+                    MainBox.css('opacity','1')  //显示主内容
+                },700);
+            },
+        });
+    }
 
     // 菜单操作
     $('#list').on("click", "#list-button", function() { 
@@ -106,19 +133,34 @@ jQuery(document).ready(function($) {
     	var href = $(this).attr("href");   //获取a标签 URL
     	var PageID = href.substring(href.lastIndexOf("?"));    // 获取 URL 问号之后的字符
     	var LoadID = PageID.substr(1); //截取前面的问号
-        var listID = $(this).data('lid');
-    	title = list[listID].title;  //获取上面 json 对应的标题
+        var listID = eval($(this).data('listid'));
     	if (PageID) {  
             //如果 PageID 存在运行下面的内容
             if (LoadID!=LID) {  
                 //判断点击连接是否当前页面，不是的话就载入内容
         		LoadData(LoadID); //载入页面内容
-        		window.history.pushState(null, title, "?"+LoadID);	//修改 url
+                title = list[listID].title;  //获取上面 json 对应的标题
+                window.history.pushState(null, title, "?"+LoadID);  //修改 url
             };
             return false; //阻止a标签跳转
     	};
 	}); 
 
+    // 提示框
+    $('body').on("click", "#warn", function() { 
+        // $(this).detach();
+        $(this).fadeOut("1000",function(){
+            $(this).detach();   //先淡出再删除提示框
+        });
+    });
+
+    /*  判断 url 载入内容，有下面的 window.addEventListener 就不需要这里了!
+    *   但是 Firefox 不能直接用 window.addEventListener 载入内容所以判断一下浏览器。
+    */
+    if (document.getBoxObjectFor&&PID) {
+        LoadData(LID);
+    };
+    
 	// 浏览器前进后退按钮
 	window.addEventListener('popstate', function(e){   
         //当点击浏览器前进后退按钮时候
